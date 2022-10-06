e639374f8629d5d4d5908b32ac89086d930f1afb
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2022-10-06 18:48:38 +0900

    add Login function

--- test.js
+++ test.js
@@ -1,24 +1,27 @@
 const root_url = `https://zoo-animal-api.herokuapp.com/animals/rand/10`;
 const root_url2 = `https://www.fishwatch.gov/api/species`;
 const form = document.querySelector("form");
+const main = document.querySelector("main");
 
 fetch(root_url2)
   .then((Response) => Response.json())
   .then((result) => {
-    const main = document.querySelector("main");
+    makeCards(result);
+  });
 
-    for (let index = 0; index < 10; index++) {
-      const element = result[index + 40];
+function makeCards(result) {
+  for (let index = 0; index < 12; index++) {
+    const element = result[index + 40];
 
-      const area = document.createElement("div");
+    const area = document.createElement("div");
 
-      area.className = "fish_info";
+    area.className = "fish_info";
 
-      const image = document.createElement("img");
+    const image = document.createElement("img");
 
-      image.src = element["Species Illustration Photo"].src;
+    image.src = element["Species Illustration Photo"].src;
 
-      area.innerHTML = ` <div class="w3-container w3-margin-bottom">
+    area.innerHTML = ` <div class="w3-container w3-margin-bottom">
       <img src="${image.src}" alt="Norway" style="width:100%; margin-top:40px" class="w3-hover-opacity">
       <div class="w3-container w3-white">
         <p style="margin: 18px 0px;"><b>${element["Species Name"]}</b></p>
@@ -26,9 +29,9 @@ fetch(root_url2)
       </div>
     </div>`;
 
-      main.appendChild(area);
-    }
-  });
+    main.appendChild(area);
+  }
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -38,15 +41,36 @@ form.addEventListener("submit", (e) => {
   const fish_name = search.value;
 
   if (fish_name) {
-    getUser(fish_name);
+    getfish(fish_name);
 
     search.value = "";
   }
 });
 
-async function getUser(name) {
-  const resp = await fetch(root_url + name);
-  const respData = await resp.json();
+async function getfish(name) {
+  const search_url = `https://www.fishwatch.gov/api/species/${name}`;
 
-  console.log(respData);
+  fetch(search_url)
+    .then((Response) => Response.json())
+    .then((result) => {
+      console.log(result);
+
+      const area = document.createElement("div");
+
+      area.className = "fish_info";
+
+      const image = document.createElement("img");
+
+      image.src = result[0]["Species Illustration Photo"].src;
+
+      area.innerHTML = ` <div class="w3-container w3-margin-bottom">
+      <img src="${image.src}" alt="Norway" style="width:100%; margin-top:40px" class="w3-hover-opacity">
+      <div class="w3-container w3-white">
+        <p style="margin: 18px 0px;"><b>${result[0]["Species Name"]}</b></p>
+        <p>${result[0].Bycatch}</p>
+      </div>
+    </div>`;
+
+      main.innerHTML = area.innerHTML;
+    });
 }
